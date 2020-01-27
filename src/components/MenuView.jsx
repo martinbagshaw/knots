e09a6590e6d153bdf34563e4b65a4f404b2765c1
Mr Bagglesworth <martinbagshaw1@gmail.com>
2020-01-27 10:18:04 +0000

    Basic Figure 8 shape morph on hover

--- src/components/MenuView.jsx
+++ src/components/MenuView.jsx
@@ -2,14 +2,10 @@
 // - shows overview of knots
 // - more like a menu though
 import React from 'react';
-import { Link } from "react-router-dom";
 import styled from 'styled-components';
-// import { darken } from 'polished';
-
 import routes from '../routes';
-import { breakpoint } from '../style/styleVariables';
+import MenuLink from './MenuLink';
 import { View } from '../style/Layout';
-import { subTitleMixin } from '../style/Typography';
 
 const MenuScreen = styled(View)`
   padding: 1rem;
@@ -17,44 +13,13 @@ const MenuScreen = styled(View)`
   flex-wrap: wrap;
 `;
 
-const KnotLink = styled(Link)`
-  width: 50%;
-  height: 50%;
-  max-width: 50vw;
-  max-height: 50vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-end;
-  align-items: center;
-  text-decoration: none;
-  > svg {
-    width: auto !important;
-    height: auto !important;
-  }
-  @media (min-width: ${breakpoint.tablet}px) {
-    width: 25%;
-    max-width: 25vw;
-  }
-  /* &:hover svg {
-    fill: darken(0.3, ${props => props.itemColour});
-  } */
-`;
-
-const KnotTitle = styled.h2`
-  ${subTitleMixin};
-  color: ${props => props.colour};
-`;
-
 const MenuView = () => {
   const knots = routes.filter(item => item.path !== '/');
   return (
   <MenuScreen>
-    {knots.map(({ path, SimpleSvg, name, colour }) => {
+    {knots.map(({ path, ...props }) => {
       return (
-      <KnotLink key={path} to={path}>
-        {SimpleSvg && <SimpleSvg />}
-        <KnotTitle colour={colour}>{name}</KnotTitle>
-      </KnotLink>
+        <MenuLink key={path} path={path} {...props} />
       )
     })}
   </MenuScreen>
