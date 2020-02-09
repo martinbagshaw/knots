0358805226658dd9ed0d673413670c9d3eb1da60
Mr Bagglesworth <martinbagshaw1@gmail.com>
2020-02-09 20:00:10 +0000

    Updates

---
+++ precache-manifest.8cb98a4babc8c1ab2ec0051b6b292219.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90fe5957e1f460eb49bbaaa238e89fee",
+    "url": "/knots/index.html"
+  },
+  {
+    "revision": "521ca2d44b9546db64b6",
+    "url": "/knots/static/css/main.4888ec48.chunk.css"
+  },
+  {
+    "revision": "0e0b10e34c035c116544",
+    "url": "/knots/static/js/2.be9cb311.chunk.js"
+  },
+  {
+    "revision": "521ca2d44b9546db64b6",
+    "url": "/knots/static/js/main.8c803e2e.chunk.js"
+  },
+  {
+    "revision": "64877d700bba18a880d4",
+    "url": "/knots/static/js/runtime-main.340a443f.js"
+  }
+]);
